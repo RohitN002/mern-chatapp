1707e9c8ad9b3bd6ada5bef901362fe2307df5ce
RohitN002 <140035253+RohitN002@users.noreply.github.com>
2024-02-05 20:18:33 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://mern-chatapp-1w85.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
